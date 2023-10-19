d5a9e967254c97abec04ebae25594ed99adddbe1
Rapunzel <148408799+Rijanaa@users.noreply.github.com>
2023-10-19 23:24:23 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template,request,flash
+
+app=Flask(__name__)
+app.secret_key="juliana123"
+@app.route("/hello")
+
+def index():
+	flash('whats your name')
+	return render_template("index.html")
+
+@app.route('/greet',methods=['POST','GET'])
+
+
+def greet():
+	flash('hi' + str(request.form[' name_input '])+", great tto see you")
+	return render_template('index.html')
+#to make file and add app : app in it  write this in cmd
+# echo >  Procfile
+#to make requirements file and get them automatically  write this in cmd
+#pip freeze > requirements.txt
+#and if any of their requerements are not statifyed @ and u cant see its version so pip install and its name and w
+#and when its installed replace @ blah blah with its version numers
+
+
